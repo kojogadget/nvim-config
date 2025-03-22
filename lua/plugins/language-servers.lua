5b45bc8a4ddd7edef7b18de991ab1c638829fea6
Kjartan Gundersen <kjartan@gunder.dev>
2025-03-22 22:51:48 +0100

    new harpoon keybind and fix vimtex options

--- lua/plugins/language-servers.lua
+++ lua/plugins/language-servers.lua
@@ -20,13 +20,13 @@ return {
       vim.g.vimtex_compiler_latexmk = {
         out_dir = "build"
       }
-      vim.opt_local.textwidth = 80
-      vim.opt_local.wrap = true
+      vim.opt["textwidth"] = 80
+      vim.opt["wrap"] = true
 
-      vim.opt_local.foldmethod = "expr"
-      vim.opt_local.foldexpr = "vimtex#fold#level(v:lnum)"
-      vim.opt_local.foldtext = "vimtex#fold#text()"
-      vim.opt_local.foldlevel = 0
+      vim.opt["foldmethod"] = "expr"
+      vim.opt["foldexpr"] = "vimtex#fold#level(v:lnum)"
+      vim.opt["foldtext"] = "vimtex#fold#text()"
+      vim.opt["foldlevel"] = 0
     end
   },
 }
